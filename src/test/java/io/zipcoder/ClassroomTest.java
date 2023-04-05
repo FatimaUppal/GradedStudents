a826da5dd3a849b4ed5dced734edaf4fcf1802a1
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-04-05 08:35:17 -0400

    finally done

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,77 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ClassroomTest {
+    @Test
+    public void getStudents() {
+        //Given
+        Student s0 = new Student("Fatima", "Uppal", new ArrayList<>(Arrays.asList(50.0, 56.9, 90.0)));
+        Student s1 = new Student("Jennifer", "Vernon", new ArrayList<>(Arrays.asList(100.0, 76.89, 45.0)));
+        Student s2 = new Student("Jacob", "Ciaffi", new ArrayList<>(Arrays.asList(99.0, 88.5, 150.0)));
+        Student[] students = {s0, s1, s2};
+        Classroom room = new Classroom(students);
+
+        //When
+        room.getStudents();
+        //Then
+        Assert.assertEquals("Fatima Uppal 66\nJennifer Vernon 74\nJacob Ciaffi 113\n", room.getStudents());
+    }
+
+    @Test
+    public void getClassAverage() {
+        //Given
+        Student s0 = new Student("Fatima", "Uppal", new ArrayList<>(Arrays.asList(50.0, 56.9, 90.0)));
+        Student s1 = new Student("Jennifer", "Vernon", new ArrayList<>(Arrays.asList(100.0, 76.89, 45.0)));
+        Student s2 = new Student("Jacob", "Ciaffi", new ArrayList<>(Arrays.asList(99.0, 88.5, 150.0)));
+
+        Student[] students = {s0, s1, s2};
+        Classroom room = new Classroom(students);
+
+        //When
+        room.getClassAverage();
+
+
+        //Then
+        Assert.assertEquals(84.3333333333333, (double)room.getClassAverage(), .02);
+    }
+
+    @Test
+    public void addStudent() {
+        //Given
+        Student s0 = new Student("Fatima", "Uppal", new ArrayList<>(Arrays.asList(50.0, 56.9, 90.0)));
+        Student s1 = new Student("Jennifer", "Vernon", new ArrayList<>(Arrays.asList(100.0, 76.89, 45.0)));
+        Student s2 = new Student("Jacob", "Ciaffi", new ArrayList<>(Arrays.asList(99.0, 88.5, 150.0)));
+
+        Student[] students = {s0, s1, s2, null};
+        Classroom room = new Classroom(students);
+
+        //When
+        Student s3 = new Student("Nina", "Mcnair", new ArrayList<>(Arrays.asList(100.0, 140.8, 99.0)));
+        room.addStudent(s3);
+
+
+        //Then
+        Assert.assertEquals("Nina", students[3].getFirstName());
+
+    }
+
+    @Test
+    public void removeStudent() {
+        //Given
+        Student s0 = new Student("Fatima", "Uppal", new ArrayList<>(Arrays.asList(50.0, 56.9, 90.0)));
+        Student s1 = new Student("Jennifer", "Vernon", new ArrayList<>(Arrays.asList(100.0, 76.89, 45.0)));
+        Student s2 = new Student("Jacob", "Ciaffi", new ArrayList<>(Arrays.asList(99.0, 88.5, 150.0)));
+
+        Student[] students = {s0, s1, s2};
+        Classroom room = new Classroom(students);
+
+        //Then
+        Assert.assertEquals(true, room.removeStudent("Jennifer", "Vernon"));
+
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,78 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StudentTest {
+    @Test
+    public void testStudentConstructor(){
+        ArrayList <Double> examScores = new ArrayList<>(Arrays.asList(58.7,94.5,102.6));
+        Student student= new Student("Fatima", "Uppal",examScores);
+
+        Assert.assertEquals("Fatima", student.getFirstName());
+        Assert.assertEquals("Uppal", student.getLastName());
+        
+    }
+    @Test
+    public void getFirstName() {
+        //Given
+        Student student = new Student("Fatima", "Uppal", null);
+
+        //Then
+        Assert.assertEquals("Fatima", student.getFirstName());
+
+    }
+
+    @Test
+    public void setFirstName() {
+        //Given
+        Student student = new Student("Fatima", "Uppal", null);
+        student.setFirstName("Fatima");
+
+        //Then
+        Assert.assertEquals("Fatima", student.getFirstName());
+    }
+
+    @Test
+    public void getLastName() {
+        //Given
+        Student student = new Student("Fatima", "Uppal", null);
+
+        //Then
+        Assert.assertEquals("Uppal", student.getLastName());
+
+
+    }
+
+    @Test
+    public void setLastName() {
+        //Given
+        Student student = new Student("Fatima", "Uppal", null);
+        student.setLastName("Smith");
+
+        //Then
+        Assert.assertEquals("Smith", student.getLastName());
+    }
+    @Test
+    public void addExamScore() {
+        //Given
+        Student student = new Student("Fatima","Uppal", new ArrayList<>());
+        student.addExamScore(58.96);
+
+        //Then
+        Assert.assertEquals(new ArrayList<>(Arrays.asList(59.0)), student.getExamScore());
+    }
+
+    @Test
+    public void getAverageScore() {
+        //Given
+        Student student = new Student("Fatima", "Uppal", new ArrayList<>(Arrays.asList(34.8, 90.4, 99.9)));
+
+        //Then
+        Assert.assertEquals(75.03, student.getAverageScore(), 0.2);
+    }
 
 }
